9f601d73438dab5f6a2cfdc96c6bf6f5f268e547
volodymyr_makarenko <hhvolodymyrhh@gmail.com>
2024-09-08 19:36:07 +0300

    edit

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { gettingData } from './js/pixabay-api.js';
 import { renderData } from './js/render-functions.js';
 //ВСТАВКА ШЛЯХУ ДЛЯ ІЗІТОСТ ЩОБ РОБИЛА
-import iconUrl from "./img/octagon.svg";
+import iconUrl from './img/octagon.svg';
 
 
 
@@ -69,7 +69,7 @@ async function addImage(InputSearch, pageGrowthJs, eventCome) {
                 messageLineHeight: "20px",
                 backgroundColor: "rgb(255,153,102)",
                 position: "topRight", 
-                iconUrl: iconUrl,
+                iconUrl,
                 imageWidth: 30,
                  });
                  // добавити скруглення для iziToast
